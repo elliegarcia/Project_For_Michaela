8ae95ecd03959ebd46f9a104081bfe3704403bb7
elliegarcia <emg96@uw.edu>
2018-02-14 09:18:26 -0800

    still fixing
    still trying to fix the problem

--- pull_names.py
+++ pull_names.py
@@ -1,26 +1,33 @@
 import sys
 directoryfile = open(sys.argv[1], "r")
 directorystring = directoryfile.read()
-directorystring = directorystring.replace('\xe2\x80\xa8', ' ')
+
+directorystring = directorystring.replace('\n', ' ')
+directorystring = directorystring.replace('..........................', '')
+directorystring = directorystring.replace('.........................', '')
 directorystring = directorystring.strip()
-directorylist = directorystring.split('\n')
-print directorylist
+directorylist = directorystring.split('OCMR')
 studentdictionary = {}
+
 for line in directorylist:
     studentlist = line.split(' ')
+    print studentlist
     if ('Col' in studentlist):
         col = studentlist.index('Col') - 1
-        colkey = ", ".join(studentlist[:2])
+        colkey = ", ".join(studentlist[3:6])
         studentdictionary[colkey] = studentlist[col]
+
     elif ('Con' in studentlist):
         con = studentlist.index('Con') - 1
-        conkey = ", ".join(studentlist[:2])
+        conkey = ", ".join(studentlist[3:6])
         studentdictionary[conkey] = studentlist[con]
+
     elif ('Dbl' in studentlist):
         dbl = studentlist.index('Dbl') - 1
-        dblkey = ", ".join(studentlist[:2])
+        dblkey = ", ".join(studentlist[3:6])
         studentdictionary[dblkey] = studentlist[dbl]
 
+studentdictionary.keys()
 # medianvaluefile = open(sys.argv[2], "r")
 # for x in range(0,14830):
 #     line = medianvaluefile.readline()
